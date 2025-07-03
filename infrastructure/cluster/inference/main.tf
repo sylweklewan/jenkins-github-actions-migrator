1022a005b646249bbd3a50422d434d15134edea4
Sylwester Lewandowski <sylwester@lewandowski.uno>
2025-07-03 23:03:45 +0200

    inference setup

--- infrastructure/cluster/inference/main.tf
+++ infrastructure/cluster/inference/main.tf
@@ -8,38 +8,57 @@ data "kubernetes_namespace" "kserve" {
 # ----------------------
 # Set default mode to RawDeployment
 # ----------------------
-resource "kubernetes_config_map" "kserve_rawdeployment_config" {
-  metadata {
-    name      = "inferenceservice-config"
-    namespace = data.kubernetes_namespace.kserve.metadata[0].name
-  }
+# resource "kubernetes_config_map" "kserve_rawdeployment_config" {
+#   metadata {
+#     name      = "inferenceservice-config"
+#     namespace = data.kubernetes_namespace.kserve.metadata[0].name
+#   }
 
-  data = {
-    deploy = yamlencode({
-      defaultDeploymentMode = "RawDeployment"
-    })
-  }
-}
+#   data = {
+#     deploy = yamlencode({
+#       defaultDeploymentMode = "RawDeployment"
+#     })
+#   }
+# }
 
 
-resource "kubernetes_manifest" "my_model_inferenceservice" {
+# resource "kubernetes_manifest" "tensorflow_flowers_inference" {
+#   manifest = {
+#     apiVersion = "serving.kserve.io/v1beta1"
+#     kind       = "InferenceService"
+#     metadata = {
+#       name      = "tensorflow-flowers"
+#       namespace = "kserve"
+#     }
+#     spec = {
+#       predictor = {
+#         tensorflow = {
+#           storageUri = "gs://kfserving-samples/models/tensorflow/flowers"
+#           resources = {
+#             limits = {
+#               memory = "2Gi"
+#             }
+#           }
+#         }
+#       }
+#     }
+#   }
+# }
+resource "kubernetes_manifest" "sklearn_iris_inference" {
   manifest = {
     apiVersion = "serving.kserve.io/v1beta1"
     kind       = "InferenceService"
     metadata = {
-      name      = "my-model"
-      namespace = "kserve"
+      name      = "sklearn-iris"
+      namespace = data.kubernetes_namespace.kserve.metadata[0].name
     }
     spec = {
       predictor = {
-        tensorflow = {
-          storageUri = "gs://kfserving-samples/models/tensorflow/flowers"
-          resources = {
-            limits = {
-              cpu    = "1"
-              memory = "2Gi"
-            }
+        model = {
+          modelFormat = {
+            name = "sklearn"
           }
+          storageUri = "gs://kfserving-examples/models/sklearn/1.0/model"
         }
       }
     }
